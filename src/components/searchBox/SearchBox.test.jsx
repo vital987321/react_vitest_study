d394f28ebefe76252f7dd3ac8c171f49fefb7c71
Vitalii Velychko <vitaliivelychko1@gmail.com>
2024-10-07 11:43:27 +0200

    restructuring SearchBox.test

--- src/components/searchBox/SearchBox.test.jsx
+++ src/components/searchBox/SearchBox.test.jsx
@@ -4,28 +4,34 @@ import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 
 describe('SearchBox', () => {
+    const renderSearchBox = () => {
+        const onChange = vi.fn()
+        const user=userEvent.setup()
+        render(<SearchBox onChange={onChange} />)
+        const inputField = screen.getByPlaceholderText(/search/i);
+        return {
+          inputField,
+          onChange,
+          user,
+        };
+    }
     it('renders component', () => {
         render(<SearchBox/>)
         const input=screen.getByPlaceholderText(/search/i)
         expect (input).toBeInTheDocument()
     })
     it('calls onChange function on enter with text', async () => {
-        const onChange=vi.fn()
-        render(<SearchBox onChange={onChange}/>)
-        const user=userEvent.setup()
-        const inputField=screen.getByPlaceholderText(/search/i)
-        await user.type(inputField, 'SomeText{Enter}')
-        expect(inputField).toHaveValue('SomeText')
+        const { inputField, onChange, user } = renderSearchBox()
+        const searchText="SearchText"
+        await user.type(inputField, searchText+'{Enter}')
+        expect(inputField).toHaveValue(searchText)
         expect(onChange).toHaveBeenCalledOnce()
     })
     it('onChange is not called on enter if input is empty', async () => {
-        const onChange=vi.fn()
-        render(<SearchBox onChange={onChange}/>)
-        const user=userEvent.setup()
-        const inputField=screen.getByPlaceholderText(/search/i)
-        await user.type(inputField, '{Enter}')
-        expect(inputField).toHaveValue('')
-        expect(onChange).not.toHaveBeenCalled()
+        const { inputField, onChange, user } = renderSearchBox();
+        await user.type(inputField, "{Enter}");
+        expect(inputField).toHaveValue('');
+        expect(onChange).not.toHaveBeenCalled();
     })
     
 })
